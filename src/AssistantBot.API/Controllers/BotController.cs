2d60c912daf3921536df7879396d24f923bdb6cc
Andrey <andrey98633@gmail.com>
2026-02-28 00:44:39 +0300

    dev: Добавил post метод в контроллер для получения обновлений от бота

--- src/AssistantBot.API/Controllers/BotController.cs
+++ src/AssistantBot.API/Controllers/BotController.cs
@@ -1,6 +1,29 @@
-﻿namespace AssistantBot.API.Controllers;
+﻿using System.Text.Json.Nodes;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Types;
 
-public class BotController
+namespace AssistantBot.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BotController : ControllerBase
 {
-    
+    private readonly ITelegramBotClient _bot;
+
+    public BotController(ITelegramBotClient bot)
+    {
+        _bot = bot;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] Update update)
+    {
+        if (update.Message is { Text: { } msgText })
+        {
+            Console.WriteLine(msgText);
+        }
+            
+        return Ok();
+    }
 }
--- src/AssistantBot.API/Program.cs
+++ src/AssistantBot.API/Program.cs
@@ -11,29 +11,31 @@ public class Program
 
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
-        
+
         builder.Services.AddSingleton<ITelegramBotClient>(provider =>
         {
-            var token= 
+            var token =
                 builder.Configuration.GetSection("BotToken").Value
                 ?? throw new Exception("No token");
-            
+
             return new TelegramBotClient(token);
         });
 
         var app = builder.Build();
 
-        using (var scope = app.Services.CreateAsyncScope())
+        await using (var scope = app.Services.CreateAsyncScope())
         {
             var bot = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
+            var hostUrl = builder.Configuration["HostUrl"]
+                          ?? throw new Exception("No URL");
+
+            await bot.SetWebhook($"{hostUrl}/api/bot");
         }
-        
-        app.UseHttpsRedirection();
-        
+
         app.UseAuthorization();
-        
+
         app.MapControllers();
-        
+
         app.Run();
     }
 }
